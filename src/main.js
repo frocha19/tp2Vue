430d82925496da12b73c7eb23318cdea42cfc7d0
Fernando <f_erchu19@hotmail.com>
2020-05-16 13:57:56 -0300

    Add Firebase

--- src/main.js
+++ src/main.js
@@ -4,12 +4,34 @@ import Vue from "vue";
 import "./plugins/bootstrap-vue";
 import App from "./App.vue";
 import router from "./router";
-import store from "./store";
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import * as firebase from "firebase/app";
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+require("firebase/firestore");
+
+Vue.config.productionTip = false;
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBKflWT_AZ1pgt6hec5SMY5ZohQTfA_GA4",
+  authDomain: "basiccrud-8a755.firebaseapp.com",
+  databaseURL: "https://basiccrud-8a755.firebaseio.com",
+  projectId: "basiccrud-8a755",
+  storageBucket: "basiccrud-8a755.appspot.com",
+  messagingSenderId: "433238462433",
+  appId: "1:433238462433:web:367710cfa750a594458ca0"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+/* Bind firebase to your Vue instance */
+Vue.prototype.$firebase = firebase;
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
-  store,
   render: h => h(App)
 }).$mount("#app");
--- src/views/Productos.vue
+++ src/views/Productos.vue
@@ -32,11 +32,75 @@ export default {
   },
   methods: {
     async getInstrumentos() {
-      const res = await fetch("/instrumentos.json");
-      const resJson = await res.json();
-      console.log(resJson);
-      this.instrumentosData = resJson.instrumentos;
+      const ids = [
+        "6FTZrSvBIkBYzgTs8Hx9",
+        "7tLIgAOu51uDdJLgTxvc",
+        "AOtWmAKEBXtbJmOgFJqK",
+        "JG78WLYLE5dp9d7TUUZ3",
+        "UDn138EcMP8dQPfwKwS8",
+        "UrQL3H09ZSPOAyPyBxOc",
+        "V6YHHCsrXKyCbG3PAtmC",
+        "ix6eSVStBna95PvD1KyE",
+        "n95rSKgEDGT2KAxjFEer",
+        "nlUQPG4YxmIynP8O5Bxd"
+      ];
+      const testCollection = [];
+      await ids.forEach(id => {
+        this.$firebase
+          .firestore()
+          .collection("instrumentosDB")
+          .doc(id)
+          .get()
+          .then(function(doc) {
+            if (doc.exists) {
+              testCollection.push(doc.data());
+              // console.log(this.instrumentosData);
+            } else {
+              // doc.data() will be undefined in this case
+              console.log("No such document!");
+            }
+          })
+          .catch(function(error) {
+            console.log("Error getting document:", error);
+          });
+      });
+      this.instrumentosData = testCollection;
+      console.log(this.instrumentosData);
+      // db.collection("instrumentosDB")
+      //   .doc("nlUQPG4YxmIynP8O5Bxd")
+      //   .get()
+      //   .then((data) => {
+      //     data.forEach((doc) => {
+      //       console.log(doc.data);
+      //       // this.instrumentosData.push({ [doc.id]: doc.data() });
+      //     });
+      //     console.log(this.instrumentosData);
+      //     // this.testCollection = data;
+      //   });
+      // console.log(this.instrumentosData);
+      // const res = await fetch("/instrumentos.json");
+      // const resJson = await res.json();
+      // this.instrumentosData = resJson.instrumentos;
+      // this.getTodo();
     }
+    // getTodo() {
+    //   const db = this.$firebase.firestore();
+    //   console.log(this.instrumentosData);
+    //   for (let i = 0; i < this.instrumentosData.length; i++) {
+    //     console.log(this.instrumentosData[i]);
+    //     db.collection("instrumentosDB").add(this.instrumentosData[i]);
+    //   }
+    //   const db = this.$firebase.firestore();
+    //   db.collection("instrumentosDB").get()
+    //   .then(snap => {
+    //     const testCollection = [];
+    //     snap.forEach(doc => {
+    //       testCollection.push({ [doc.id]: doc.data() });
+    //     });
+    //     this.testCollection = testCollection;
+    //     console.log(testCollection);
+    //   });
+    // }
   }
 };
 </script>
