d703583bd8d719b25afe3175e7f226b4c165175b
Fernando <f_erchu19@hotmail.com>
2020-05-16 14:22:48 -0300

    Fixing Code

--- src/components/NavBarTop.vue
+++ src/components/NavBarTop.vue
@@ -7,9 +7,9 @@
 
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
-          <b-nav-item href="/" active="true">Home</b-nav-item>
-          <b-nav-item href="/productos" active="true">Productos</b-nav-item>
-          <b-nav-item href="/ubicacion" active="true">Donde Estamos</b-nav-item>
+          <b-nav-item href="/">Home</b-nav-item>
+          <b-nav-item href="/productos">Productos</b-nav-item>
+          <b-nav-item href="/ubicacion">Donde Estamos</b-nav-item>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
--- src/views/Productos.vue
+++ src/views/Productos.vue
@@ -27,80 +27,32 @@ export default {
   },
   data() {
     return {
-      instrumentosData: []
+      instrumentosData: [],
+      ids: []
     };
   },
   methods: {
     async getInstrumentos() {
-      const ids = [
-        "6FTZrSvBIkBYzgTs8Hx9",
-        "7tLIgAOu51uDdJLgTxvc",
-        "AOtWmAKEBXtbJmOgFJqK",
-        "JG78WLYLE5dp9d7TUUZ3",
-        "UDn138EcMP8dQPfwKwS8",
-        "UrQL3H09ZSPOAyPyBxOc",
-        "V6YHHCsrXKyCbG3PAtmC",
-        "ix6eSVStBna95PvD1KyE",
-        "n95rSKgEDGT2KAxjFEer",
-        "nlUQPG4YxmIynP8O5Bxd"
-      ];
       const testCollection = [];
-      await ids.forEach(id => {
+      await this.$firebase
+        .firestore()
+        .collection("instrumentosDB")
+        .get()
+        .then(data => {
+          data.forEach(doc => {
+            this.ids.push(doc.id);
+          });
+        });
+      await this.ids.forEach(id => {
         this.$firebase
           .firestore()
           .collection("instrumentosDB")
           .doc(id)
           .get()
-          .then(function(doc) {
-            if (doc.exists) {
-              testCollection.push(doc.data());
-              // console.log(this.instrumentosData);
-            } else {
-              // doc.data() will be undefined in this case
-              console.log("No such document!");
-            }
-          })
-          .catch(function(error) {
-            console.log("Error getting document:", error);
-          });
+          .then(doc => testCollection.push(doc.data()));
       });
       this.instrumentosData = testCollection;
-      console.log(this.instrumentosData);
-      // db.collection("instrumentosDB")
-      //   .doc("nlUQPG4YxmIynP8O5Bxd")
-      //   .get()
-      //   .then((data) => {
-      //     data.forEach((doc) => {
-      //       console.log(doc.data);
-      //       // this.instrumentosData.push({ [doc.id]: doc.data() });
-      //     });
-      //     console.log(this.instrumentosData);
-      //     // this.testCollection = data;
-      //   });
-      // console.log(this.instrumentosData);
-      // const res = await fetch("/instrumentos.json");
-      // const resJson = await res.json();
-      // this.instrumentosData = resJson.instrumentos;
-      // this.getTodo();
     }
-    // getTodo() {
-    //   const db = this.$firebase.firestore();
-    //   console.log(this.instrumentosData);
-    //   for (let i = 0; i < this.instrumentosData.length; i++) {
-    //     console.log(this.instrumentosData[i]);
-    //     db.collection("instrumentosDB").add(this.instrumentosData[i]);
-    //   }
-    //   const db = this.$firebase.firestore();
-    //   db.collection("instrumentosDB").get()
-    //   .then(snap => {
-    //     const testCollection = [];
-    //     snap.forEach(doc => {
-    //       testCollection.push({ [doc.id]: doc.data() });
-    //     });
-    //     this.testCollection = testCollection;
-    //     console.log(testCollection);
-    //   });
-    // }
   }
 };
 </script>
